fe7d9675949fe82f3bd3033cb2f0cbf8f5b9126d
Eduarda Vitorino Nagatomo <125457175+nagatomox@users.noreply.github.com>
2023-06-01 22:29:50 -0300

    Update testeapi_test.js

--- testeapi_test.js
+++ testeapi_test.js
@@ -4,8 +4,8 @@ import {
     assertArrayIncludes,
 } from "https://deno.land/std/testing/asserts.ts"
 
-let aluno = 'Cróvis José'
-let professores = ['João', 'Isabelli', 'Roberto', 'Kevin']
+let aluno = 'Marcelo'
+let professores = ['João', 'Janette', 'Matilde', 'Cleide']
 
 Deno.test("Teste de assertions (API)", () => {
     assertEquals(aluno, "José") //Verifica se dentro da variável aluno possui 'José'
